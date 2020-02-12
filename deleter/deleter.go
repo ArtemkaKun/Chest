d1042870e8669e099e86f2a01e5ee4f283bb279c
ArtemkaKun <zumikq78@gmail.com>
2020-02-12 17:18:49 +0100

    Created deleter module (but not work, need to fix file search func)

---
+++ deleter/deleter.go
@@ -0,0 +1,114 @@
+package deleter
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func getService() (*drive.Service, error) {
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		fmt.Printf("Unable to read credentials.json file. Err: %v\n", err)
+		return nil, err
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, drive.DriveFileScope)
+
+	if err != nil {
+		return nil, err
+	}
+
+	client := getClient(config)
+
+	service, err := drive.New(client)
+
+	if err != nil {
+		fmt.Printf("Cannot create the Google Drive service: %v\n", err)
+		return nil, err
+	}
+
+	return service, err
+}
+
+func CheckOld() {
+	service, err := getService()
+	if err != nil {
+		log.Fatalf("Unable to retrieve Drive client: %v", err)
+	}
+
+	r, err := service.Files.List().Q("'root' in parents").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve files: %v", err)
+	}
+	fmt.Println("Files:")
+	if len(r.Files) == 0 {
+		fmt.Println("No files found.")
+	} else {
+		for _, i := range r.Files {
+			fmt.Printf("%v (%vs )\n", i.Name, i.Id)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,35 +1,29 @@
 package main
 
-import (
-	"./packer"
-	starter "./serverStarter"
-	stoper "./serverStoper"
-	"./uploader"
-	"flag"
-	"fmt"
-	"sync"
-)
+import "./deleter"
 
 func main() {
-	new_screen_id := flag.Int("nssid", 0, "Make new server-stoper and server-starter scripts (place here screen ID)")
-	new_stoper_time := flag.Int("nstime", 30, "Make new server-stoper script (place here time before stop)")
-	new_server_path := flag.String("sf", "", "Make new packer script (place here path to your Minecraft server files)")
-	flag.Parse()
+	//new_screen_id := flag.Int("ns", 0, "Make new server-stoper and server-starter scripts (place here screen ID)")
+	//new_stoper_time := flag.Int("d", 30, "Make new server-stoper script (place here time before stop)")
+	//new_server_path := flag.String("sf", "", "Make new packer script (place here path to your Minecraft server files)")
+	//flag.Parse()
+	//
+	//fmt.Println("The server will be stopped after delay")
+	//stoper.StopServer(*new_screen_id, *new_stoper_time)
+	//
+	//fmt.Println("The server was stopped. Process of backup making was started...")
+	//new_pack := packer.MakePack(*new_server_path)
+	//
+	//fmt.Println("Backup file was successfully created! Process of uploading was started...")
+	//var upload_goroutine sync.WaitGroup
+	//upload_goroutine.Add(1)
+	//go uploader.UploadPack(new_pack, &upload_goroutine)
+	//
+	//fmt.Println("Starting the server...")
+	//starter.StartServer(*new_screen_id)
+	//fmt.Println("The server started")
+	//
+	//upload_goroutine.Wait()
 
-	fmt.Println("The server will be stopped after delay")
-	stoper.StopServer(*new_screen_id, *new_stoper_time)
-
-	fmt.Println("The server was stopped. Process of backup making was started...")
-	new_pack := packer.MakePack(*new_server_path)
-
-	fmt.Println("Backup file was successfully created! Process of uploading was started...")
-	var upload_goroutine sync.WaitGroup
-	upload_goroutine.Add(1)
-	go uploader.UploadPack(new_pack, &upload_goroutine)
-
-	fmt.Println("Starting the server...")
-	starter.StartServer(*new_screen_id)
-	fmt.Println("The server started")
-
-	upload_goroutine.Wait()
+	deleter.CheckOld()
 }
